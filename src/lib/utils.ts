1626f179a1c630eec381b62e3177e3ef7e7e8e8e
marceloamellopaixao <marceloamellopaixao@gmail.com>
2026-01-29 00:50:46 -0300

    feat(utils): adicionar comentário explicativo para a função de mesclagem de classes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Utility function to merge class names with Tailwind CSS support
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
