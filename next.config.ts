78e02105dfd89ec34ce9b6dd083e5e9082ea70ac
marceloamellopaixao <marceloamellopaixao@gmail.com>
2026-04-04 01:25:25 -0300

    feat: adicionar redirecionamentos para a página do cofre

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,25 @@ import { withSentryConfig } from "@sentry/nextjs";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/porquinho/novo",
+        destination: "/piggy-bank/new",
+        permanent: true,
+      },
+      {
+        source: "/porquinho/:slug",
+        destination: "/piggy-bank/:slug",
+        permanent: true,
+      },
+      {
+        source: "/porquinho",
+        destination: "/piggy-bank",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withSentryConfig(nextConfig, {
