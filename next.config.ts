dc42d2e5ae714232b1b8fc0504a5899cb52eb8b3
marceloamellopaixao <marceloamellopaixao@gmail.com>
2026-06-02 15:33:27 -0300

    feat: adicionar configuração do turbopack no Next.js

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import { withSentryConfig } from "@sentry/nextjs";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: process.cwd(),
+  },
   async redirects() {
     return [
       {
